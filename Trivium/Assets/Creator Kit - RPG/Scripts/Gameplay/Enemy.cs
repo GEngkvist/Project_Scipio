2125b7596c51f402822dbd15f254b24fb0cc25a9
GEngkvist <gengkvist25@yahoo.com>
2020-03-08 20:13:18 -0500

    First Attack Attemps
    Attempted to implement the attack function on the character in the first level, Should have tried in the tutorial level.

---
+++ Trivium/Assets/Creator Kit - RPG/Scripts/Gameplay/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using RPGM.Gameplay;
+using UnityEngine.U2D;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Damage TAKEN!");
+    }
+}
---
+++ Trivium/Assets/Creator Kit - RPG/Scripts/Gameplay/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using RPGM.Gameplay;
+using UnityEngine.U2D;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos = null;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+
+    void update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+
+                timeBtwAttack = startTimeBtwAttack;
+            }
+
+        }
+
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+
+
+    } 
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
