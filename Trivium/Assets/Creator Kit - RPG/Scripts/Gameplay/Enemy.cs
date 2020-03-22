9e377b862043180bf799e519008b23e772379654
xMrPlaymaker <51717657+xMrPlaymaker@users.noreply.github.com>
2020-03-22 17:23:21 -0500

    Enemy Ai

--- Trivium/Assets/Creator Kit - RPG/Scripts/Gameplay/Enemy.cs
+++ Trivium/Assets/Creator Kit - RPG/Scripts/Gameplay/Enemy.cs
@@ -11,11 +11,17 @@ public class Enemy : MonoBehaviour
 
     public int maxHealth = 100;
     int currentHealth;
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
 
 
     void Start()
     {
         currentHealth = maxHealth;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     public void TakeDamage(int damage)
@@ -30,7 +36,25 @@ public class Enemy : MonoBehaviour
         }
     }
 
-       
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+      //  Debug.Log(direction);
+       // float angle = (float)Math.Atan2(direction.x, direction.y)*Mathf.Rad2Deg;
+       // rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
     void Die()  
     {
         Debug.Log("Enemy Died!");
